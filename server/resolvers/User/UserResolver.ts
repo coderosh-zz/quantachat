f3684db726b13e16ded84efd170fa712e6e46d9c
acharyaroshanji <acharyaroshan2357@gmail.com>
2020-07-02 17:40:10 +0545

    feat(server): add and remove friends

--- server/resolvers/User/UserResolver.ts
+++ server/resolvers/User/UserResolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Arg, Authorized, Mutation, Ctx } from 'type-graphql';
 import { isValidObjectId } from 'mongoose';
 import User, { UserClass } from '../../entities/User';
 import { Context } from '../../Context';
+import { ApolloError } from 'apollo-server-express';
 
 @Resolver(() => UserClass)
 class UserResolver {
@@ -21,6 +22,62 @@ class UserResolver {
   @Query(() => UserClass, { nullable: true })
   async me(@Ctx() context: Context): Promise<UserClass | undefined> {
     return context.getUser();
+  }
+
+  @Authorized()
+  @Mutation(() => Boolean)
+  async addFriend(
+    @Arg('id')
+    id: string,
+    @Ctx() context: Context
+  ): Promise<boolean> {
+    try {
+      const friend = await User.findById(id);
+      if (!friend) throw new ApolloError('User not found');
+      const me = await User.findById(context.currentUser._id);
+
+      if (me!.friends!.includes(id as any)) {
+        return true;
+      }
+
+      me!.friends!.push(friend.id);
+      friend.friends!.push(me!._id);
+
+      await me!.save();
+      return true;
+    } catch (e) {
+      throw new ApolloError(e);
+    }
+  }
+
+  @Authorized()
+  @Mutation(() => UserClass)
+  async removeFriend(
+    @Arg('id')
+    id: string,
+    @Ctx() context: Context
+  ): Promise<UserClass> {
+    try {
+      const friend = await User.findByIdAndUpdate(
+        id,
+        {
+          $pull: { friends: context.currentUser._id },
+        },
+        { new: true }
+      );
+
+      if (!friend) throw new ApolloError('User not found');
+
+      await User.findByIdAndUpdate(
+        context.currentUser._id,
+        { $pull: friend._id },
+        { new: true }
+      );
+
+      return friend;
+    } catch (e) {
+      throw new ApolloError(e);
+    }
   }
 
   @Authorized()
